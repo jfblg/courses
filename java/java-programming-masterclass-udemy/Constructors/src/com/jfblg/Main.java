268e97891de9fa627ecd0a9d2ff745b9b4e5cd9a
Janus Frantisek, INI-DOS-DPS-EAS <Frantisek.Janus@swisscom.com>
2019-03-22 18:01:21 +0100

    add new code

--- java/java-programming-masterclass-udemy/Constructors/src/com/jfblg/Main.java
+++ java/java-programming-masterclass-udemy/Constructors/src/com/jfblg/Main.java
@@ -4,7 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Account account = new Account();
-        System.out.println(account.getCustomerPhoneNumber());
+//        Account account = new Account();
+//        System.out.println(account.getCustomerPhoneNumber());
+
+        Dog dog = new Dog("doberman", 1, 2, 3, 4, 5, 6, "NiceOne");
+        dog.eat();
     }
 }
